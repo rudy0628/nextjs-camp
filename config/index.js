54cf43ca8a2002f6bb8b66c92493933ec86f2a5f
rudy0628 <s3352250zz@gmail.com>
2022-02-07 14:58:04 +0800

    change domain url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
 	? 'http://localhost:3000'
-	: 'https://nextjs-camp-dtznbxpi1-rudy0628.vercel.app';
+	: 'https://nextjs-camp-mocha.vercel.app';
