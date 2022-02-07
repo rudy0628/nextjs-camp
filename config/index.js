e716a1aff7ba3b3407e377588ad5f94f7aed114a
rudy0628 <s3352250zz@gmail.com>
2022-02-07 16:41:18 +0800

    updated env

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : process.env.Vercel_URL;
+export const server = dev ? 'http://localhost:3000' : process.env.VERCEL_URL;
