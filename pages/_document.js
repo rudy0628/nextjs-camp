43ae0d7878358d1b110469849e09fe440b8dfa17
rudy0628 <s3352250zz@gmail.com>
2022-02-08 13:10:45 +0800

    change next version

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-	return (
-		<Html>
-			<Head />
-			<body>
-				<div className="flash-text" />
-				<div className="backdrop" />
-				<div className="overlay" />
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<div className="flash-text" />
+					<div className="backdrop" />
+					<div className="overlay" />
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
+
+// export default function Document() {
+// 	return (
+// 		<Html>
+// 			<Head />
+// 			<body>
+// 				<div className="flash-text" />
+// 				<div className="backdrop" />
+// 				<div className="overlay" />
+// 				<Main />
+// 				<NextScript />
+// 			</body>
+// 		</Html>
+// 	);
+// }
